083a6ac5d9dad3901ca922e991c1dad72dfb29b5
Andrew Koval <heyandydude@gmail.com>
2015-02-25 18:28:57 +0200

    add product info & characteristic

--- js/animate.js
+++ js/animate.js
@@ -42,15 +42,3 @@ $(function(){
     }
 
 });
-
-
-wow = new WOW(
-    {
-        animateClass: 'animated',
-        offset:       100,
-        callback:     function(box) {
-            console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-        }
-    }
-);
-wow.init();
